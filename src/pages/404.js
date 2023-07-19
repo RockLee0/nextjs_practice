25a4069521193ac25988ddb0df10bf6a47e08557
Rocklee0 <mshakil3847@gmail.com>
2023-07-20 02:37:15 +0600

    learned to make 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <img src="https://w7.pngwing.com/pngs/336/743/png-transparent-error-404-404-error.png" alt="" height="100%" width="100%" />
+        </div>
+    );
+};
+
+export default Error;
