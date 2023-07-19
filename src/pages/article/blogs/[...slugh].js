f4b93702b64dfe80b927770b9e191c7127b41f1a
Rocklee0 <mshakil3847@gmail.com>
2023-07-20 02:33:49 +0600

    added filtering route

---
+++ src/pages/article/blogs/[...slugh].js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const query = () => {
+    return (
+        <div>
+            <h1> this will shown after blogs/id/ anything </h1>
+            <h1> it is called filtering routes </h1>
+        </div>
+    );
+};
+
+export default query;
